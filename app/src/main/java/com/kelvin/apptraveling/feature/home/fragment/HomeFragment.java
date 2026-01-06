a3060d858b21968f1f4fedbb42991d9545b15e60
Kelvin <Kelvinor29@gmail.com>
2026-01-06 15:54:17 -0600

    feat(home): implement navigation to map fragment

--- app/src/main/java/com/kelvin/apptraveling/feature/home/fragment/HomeFragment.java
+++ app/src/main/java/com/kelvin/apptraveling/feature/home/fragment/HomeFragment.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,11 +13,13 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.kelvin.apptraveling.R;
 import com.kelvin.apptraveling.data.api.API;
 import com.kelvin.apptraveling.data.models.Hotel;
 import com.kelvin.apptraveling.data.models.HotelResponse;
 import com.kelvin.apptraveling.databinding.FragmentHomeBinding;
 import com.kelvin.apptraveling.feature.home.adapter.HotelAdapter;
+import com.kelvin.apptraveling.feature.home.fragment.map.MapFragment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,81 +31,84 @@ import retrofit2.Response;
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private HotelAdapter hotelAdapter;
     private String username;
-    private HotelAdapter adapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
 
-        // Recibe datos del fragment de login
         getParentFragmentManager().setFragmentResultListener("userDataFromLogin", this, ((requestKey, result) -> {
             username = result.getString("username");
-
-            Log.d("Depurando", "Data received: Username: " + username);
-            Snackbar.make(binding.getRoot(), "Name: " + username, Snackbar.LENGTH_LONG).show();
+            Snackbar.make(binding.getRoot(), "Bienvenido: " + username, Snackbar.LENGTH_LONG).show();
         }));
 
         return binding.getRoot();
-
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        adapter = new HotelAdapter(new ArrayList<>());
+        hotelAdapter = new HotelAdapter(new ArrayList<>(), this::navigateToMap);
 
         binding.rvHotels.setLayoutManager(new LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false));
-        binding.rvHotels.setAdapter(adapter);
+        binding.rvHotels.setAdapter(hotelAdapter);
 
         fetchHotels();
-
     }
+
+    private void navigateToMap(Hotel hotel) {
+        MapFragment mapFragment = MapFragment.newInstance(hotel);
+
+        FrameLayout mapContainer = requireActivity().findViewById(R.id.fl_mapFragment);
+
+        mapContainer.setVisibility(View.VISIBLE);
+
+        requireActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fl_mapFragment, mapFragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
 
     private void fetchHotels() {
         API api = new API(API.BASE_URL);
         Call<HotelResponse> call = api.service.getHotels();
 
-        // Mostrar shimmer
         call.enqueue(new Callback<HotelResponse>() {
             @Override
-            public void onResponse(Call<HotelResponse> call, Response<HotelResponse> response) {
-
-                if (response.isSuccessful()) {
-
-                    // Validar si se encontraron resultados
-                    if (response.body().getResults() == null) {
-                        binding.llNullHotels.setVisibility(View.VISIBLE);
-                    }
-
+            public void onResponse(@NonNull Call<HotelResponse> call, @NonNull Response<HotelResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
                     List<Hotel> hotels = response.body().getResults();
 
-                    adapter.hotelList.clear();
-                    adapter.hotelList.addAll(hotels);
-                    adapter.notifyDataSetChanged();
-
-                    adapter.setDataLoaded(true);
-
+                    if (hotels == null || hotels.isEmpty()) {
+                        binding.llNullHotels.setVisibility(View.VISIBLE);
+                    } else {
+                        binding.llNullHotels.setVisibility(View.GONE);
+                        hotelAdapter.hotelList.clear();
+                        hotelAdapter.hotelList.addAll(hotels);
+                        hotelAdapter.setDataLoaded(true);
+                    }
                 } else {
                     binding.llNullHotels.setVisibility(View.VISIBLE);
                 }
             }
 
             @Override
-            public void onFailure(Call<HotelResponse> call, Throwable t) {
-                binding.llNullHotels.setVisibility(View.VISIBLE);
+            public void onFailure(@NonNull Call<HotelResponse> call, @NonNull Throwable t) {
+                if (binding != null) {
+                    binding.llNullHotels.setVisibility(View.VISIBLE);
+                }
             }
         });
     }
-
 
     @Override
     public void onDestroyView() {
